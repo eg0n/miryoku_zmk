e39da606f225b753bea3b803d33c517054f1be7d
Adam Brown <egon@fivefuses.com>
2025-07-11 19:12:25 -0600

    Revert "Move config settings in-repo"
    This reverts commit 8f5435378e563f580aa1369ec4bba030fc9bfea7.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,3 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS_COLEMAK
-#define MIRYOKU_EXTRA_QWERTY
-#define MIRYOKU_TAP_COLEMAK
-#define MIRYOKU_CLIPBOARD_MAC
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
