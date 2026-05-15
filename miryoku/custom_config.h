935582b201d6ed2a91b54072ad36993469856f9d
Adam Brown <egon@fivefuses.com>
2026-05-14 22:17:53 -0600

    Fix up totem-specific custom_config so corne builds

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,22 +1,22 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_CLIPBOARD_MAC
-#define MIRYOKU_ALPHAS_COLEMAK
-#define MIRYOKU_TAP_COLEMAK
-#define MIRYOKU_EXTRA_QWERTY
-#define MIRYOKU_KLUDGE_GLOBALSHIFTFUNCTIONS
-#define MIRYOKU_KLUDGE_DOUBLETAPBOOT
-
+#if defined (RGBLED_WIDGET_BEHAVIORS)
 #include <behaviors/rgbled_widget.dtsi>
-#define MIRYOKU_LAYERMAPPING_BASE( \
+#define IND_CON &ind_con
+#define IND_BAT &ind_bat
+#else
+#define IND_CON &none
+#define IND_BAT &none
+#endif
+
+#define MIRYOKU_LAYOUTMAPPING_TOTEM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-     K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
-     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
-&ind_bat  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &ind_con \
-               K32  K33  K34       K35  K36  K37
-
+             K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+             K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+    IND_CON  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  IND_BAT \
+                       K32  K33  K34       K35  K36  K37
